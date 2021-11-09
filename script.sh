56bf25c8bf5484bf79fc339232ce8dd9b6e5ea14
Parsam Elizbaryan <parsam.elizbaryan@mail.ru>
2021-11-10 00:27:41 +0400

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+export AWS_ACCESS_KEY_ID=AKIA5RFRJPVWJ5D5HHZI
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_DEFAULT_REGION=us-west-2
+
+num=1
+bucket_name="bucket"
+#pwd=$(pwd)
+echo "-------------------------------------------------------------------------------------------------------------" > outfile
+echo -e "Name\t\tRegion\t\tCreationDate\t\t\tNumberOfFiles\t\tSize\t\tLastModified" >> outfile
+echo "-------------------------------------------------------------------------------------------------------------" >> outfile
+cat outfile
+
+while [ -n "$bucket_name" ]; do
+
+    bucket_name=`aws s3api list-buckets --query "Buckets[].Name" --output text | awk -v i=$num '{print $i;}'`
+    creation_date=`aws s3api list-buckets --query "Buckets[].CreationDate" --output text | awk -v n=$num '{print $n;}'`
+
+    if [[ -n "$bucket_name" ]]; then
+        region=`aws s3api get-bucket-location --bucket $bucket_name --output text`
+        last_modified=`aws s3 ls $bucket_name --recursive | sort | tail -n 1 | awk '{print $1}'`
+
+        if [[ $(aws s3api list-objects --bucket $bucket_name) ]]; then
+          number_of_files=`aws s3api list-objects --bucket $bucket_name --output text --query "[length(Contents[])]"`
+          size=`aws s3api list-objects --bucket $bucket_name --output text --query "[sum(Contents[].Size)]"`
+        else
+          number_of_files=""
+          size=""
+        fi
+    else
+        region=""
+    fi
+
+    #bucket_name=`docker run --rm -it -v $pwd/.aws:/root/.aws amazon/aws-cli s3api list-buckets --query "Buckets[].Name" --output text | awk -v i=$num '{print $i;}'`
+    #region=`docker run --rm -it -v $pwd/.aws:/root/.aws amazon/aws-cli s3api get-bucket-location --bucket $bucket_name --output text`
+    #creation_date=`docker run --rm -it -v $pwd/.aws:/root/.aws amazon/aws-cli s3api list-buckets --query "Buckets[].CreationDate" --output text | awk -v n=$num '{print $n;}'`
+
+    output="$bucket_name\t$region\t$creation_date\t\t$number_of_files\t\t$size\t\t$last_modified"
+
+
+    echo -e $output >> outfile
+    cat outfile | tail -1
+
+    num=$((num+1))
+done
+
+
