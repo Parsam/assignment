220075665a0ae7ac4aedcd47b880fb216f038feb
parsam <parsam.elizbaryan@mail.ru>
2021-11-10 17:34:58 +0400

    add functions

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# assignment
+## **This is a command line utility that returns information over all S3 buckets in an Amazon account.**
+
+
+-------------------------------------
+
+
+#### First you have to create aws_creds file with AWS credentials in this folder.
+
+#### Then source script.sh file in your terminal session.
+$source script.sh
+
+
+--------------------------------------
+
+#### The following functions are available
+
+
+`list_buckets` - list all S3 buckets in AWS account
+
+`list_objects` - list objects in bucket (require an argument)
+
+`describe_bucket` - show detailed information about specific bucket (require an argument)
+
+`describe_all_buckets` - show detailed information about all bucket in AWS account
--- script.sh
+++ script.sh
@@ -2,47 +2,69 @@
 
 source aws_creds
 
-num=1
-bucket_name="bucket"
-#pwd=$(pwd)
-echo "-----------------------------------------------------------------------------------------------------" > outfile
-echo -e "Name\t\tRegion\t\tCreationDate\t\t\tNumberOfFiles\tSize\tLastModified" >> outfile
-echo "-----------------------------------------------------------------------------------------------------" >> outfile
-cat outfile
+function list_buckets()
+{
+  aws s3 ls | awk '{print $3}'
+}
 
-while [ -n "$bucket_name" ]; do
+function list_objects()
+{
+  if [[ -n "$1" ]]; then
+    echo "-----------------------------------------"
+    echo -e "CreationDate \t\t Size ObjectName"
+    echo "-----------------------------------------"
+    aws s3 ls --human-readable --recursive $1
+  else
+    echo "Enter S3 bucket name"
+  fi
+}
 
-    bucket_name=`aws s3api list-buckets --query "Buckets[].Name" --output text | awk -v i=$num '{print $i;}'`
-    creation_date=`aws s3api list-buckets --query "Buckets[].CreationDate" --output text | awk -v n=$num '{print $n;}'`
+function dscrb() {
+  creation_date=`aws s3 ls |grep $bucket_name | awk '{print $1}'`
+  region=`aws s3api get-bucket-location --bucket $bucket_name --output text`
+  last_modified=`aws s3 ls $bucket_name --recursive | sort | tail -n 1 | awk '{print $1}'`
+  if [[ $(aws s3api list-objects --bucket $bucket_name) ]]; then
+    number_of_files=`aws s3api list-objects --bucket $bucket_name --output text --query "[length(Contents[])]"`
+    size=`aws s3api list-objects --bucket $bucket_name --output text --query "[sum(Contents[].Size)]"`
+  else
+    number_of_files=""
+    size=""
+  fi
 
+  output="$bucket_name\t$region\t$creation_date\t$number_of_files\t\t$size\t$last_modified"
+  echo -e $output
+}
+
+function describe_bucket()
+{
+  if [[ -n "$1" ]]; then
+    echo "-------------------------------------------------------------------------------------"
+    echo -e "Name\t\tRegion\t\tCreationDate\tNumberOfFiles\tSize\tLastModified"
+    echo "-------------------------------------------------------------------------------------"
+    bucket_name=$1
+    dscrb
+  else
+    echo "Enter S3 bucket name"
+  fi
+}
+
+function describe_all_buckets()
+{
+  num=1
+  bucket_name="bucket"
+  echo "-------------------------------------------------------------------------------------"
+  echo -e "Name\t\tRegion\t\tCreationDate\tNumberOfFiles\tSize\tLastModified"
+  echo "-------------------------------------------------------------------------------------"
+
+  while [ -n "$bucket_name" ]; do
+    bucket_name=`aws s3 ls | awk '{print $3}' | sed "${num}q;d"`
     if [[ -n "$bucket_name" ]]; then
-        region=`aws s3api get-bucket-location --bucket $bucket_name --output text`
-        last_modified=`aws s3 ls $bucket_name --recursive | sort | tail -n 1 | awk '{print $1}'`
-
-        if [[ $(aws s3api list-objects --bucket $bucket_name) ]]; then
-          number_of_files=`aws s3api list-objects --bucket $bucket_name --output text --query "[length(Contents[])]"`
-          size=`aws s3api list-objects --bucket $bucket_name --output text --query "[sum(Contents[].Size)]"`
-        else
-          number_of_files=""
-          size=""
-        fi
+      dscrb
     else
-        region=""
+      break
     fi
-
-    #bucket_name=`docker run --rm -it -v $pwd/.aws:/root/.aws amazon/aws-cli s3api list-buckets --query "Buckets[].Name" --output text | awk -v i=$num '{print $i;}'`
-    #region=`docker run --rm -it -v $pwd/.aws:/root/.aws amazon/aws-cli s3api get-bucket-location --bucket $bucket_name --output text`
-    #creation_date=`docker run --rm -it -v $pwd/.aws:/root/.aws amazon/aws-cli s3api list-buckets --query "Buckets[].CreationDate" --output text | awk -v n=$num '{print $n;}'`
-
-    output="$bucket_name\t$region\t$creation_date\t$number_of_files\t\t$size\t$last_modified"
-
-
-    echo -e $output >> outfile
-    cat outfile | tail -1
-
     num=$((num+1))
-done
-
-
+  done
+}
 
 
